40fe779b369c4b5d2aa8377445d200d3526f1eee
saúl z mtz <97975590+szmtz@users.noreply.github.com>
2023-10-26 17:22:44 +0000

    Add initial JS EventListener

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    //store all components in HTML in our JS
+    let clear = document.querySelector("#clear-btn"); 
+    let equal = document.querySelector(".equal"); 
+    let decimal = document.querySelector(".decimal"); 
+
+    
+}
+
