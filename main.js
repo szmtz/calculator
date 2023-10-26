8f85cf9634ac4df5d96900d56c7c5f8a755d0490
saúl z mtz <97975590+szmtz@users.noreply.github.com>
2023-10-26 20:39:38 +0000

    Add EventListener

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
-    //store all components in HTML in our JS
-    let clear = document.querySelector("#clear-btn"); 
-    let equal = document.querySelector(".equal"); 
-    let decimal = document.querySelector(".decimal"); 
+let operator = "";
+let previousValue = "";
+let currentValue = "";
 
-    
+document.addEventListener("DOMContentLoaded", function () {
+  //store all components in HTML in our JS
+  let clear = document.querySelector("#clear-btn");
+  let equal = document.querySelector(".equal");
+  let decimal = document.querySelector(".decimal");
+
+  let numbers = document.querySelectorAll(".number");
+  let operators = document.querySelectorAll(".operator");
+
+  let previousScreen = document.querySelector(".previous");
+  let currentScreen = document.querySelector(".current");
+
+  numbers.forEach((number) =>
+    number.addEventListener("click", function () {
+      handleNumber(e.target.textContent);
+    })
+  );
+});
+
+function handleNumber(num) {
+  currentValue += number;
 }
-
