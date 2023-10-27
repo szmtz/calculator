6a4d7935b3c85517d90c9d2831989a129682128e
saúl z mtz <97975590+szmtz@users.noreply.github.com>
2023-10-27 16:37:00 +0000

    Finalize Calculator Project

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let operator = '';
-let previousValue = '';
-let currentValue = '';
+let operator = "";
+let previousValue = "";
+let currentValue = "";
 
 document.addEventListener("DOMContentLoaded", function () {
   //store all components in HTML in our JS
@@ -14,75 +14,83 @@ document.addEventListener("DOMContentLoaded", function () {
   let previousScreen = document.querySelector(".previous");
   let currentScreen = document.querySelector(".current");
 
-numbers.forEach((number) =>
+  numbers.forEach((number) =>
     number.addEventListener("click", function (e) {
       handleNumber(e.target.textContent);
       currentScreen.textContent = currentValue;
-    }))
+    })
+  );
 
-operators.forEach((op) =>
+  operators.forEach((op) =>
     op.addEventListener("click", function (e) {
       handleOperator(e.target.textContent);
       previousScreen.textContent = previousValue + " " + operator;
       currentScreen.textContent = currentValue;
-     }))
+    })
+  );
 
+  clear.addEventListener("click", function () {
+    previousValue = "";
+    currentValue = "";
+    operator = "";
+    previousScreen.textContent = currentValue;
+    currentScreen.textContent = currentValue;
+  });
 
-clear.addEventListener("click", function(){
-  previousValue = '';
-  currentValue = '';
-  operator = '';
-  previousScreen.textContent = currentValue;
-  currentScreen.textContent = currentValue;
-  })
+  equal.addEventListener("click", function () {
+    if (currentValue != "" && previousValue != "") {
+      calculate();
+      previousScreen.textContent = "";
+      if (previousValue.length <= 5) {
+        currentScreen.textContent = previousValue;
+      } else {
+        currentScreen.textContent = previousValue.slice(0, 5) + "...";
+      }
+    }
+  });
 
-  
-  equal.addEventListener("click", function(){
-    calculate()
-    previousScreen.textContent = ''; 
-    currentScreen.textContent = previousValue; 
-  })
-})
-
+  decimal.addEventListener("click", function () {
+    addDecimal();
+  });
+});
 
 function handleNumber(num) {
-   if (currentValue.length <= 5) {
-    currentValue += num; 
+  if (currentValue.length <= 5) {
+    currentValue += num;
   }
 }
 
-function handleOperator(op){
-  operator = op; 
-  previousValue = currentValue; 
-  currentValue = ""; 
+function handleOperator(op) {
+  operator = op;
+  previousValue = currentValue;
+  currentValue = "";
 }
 
+function calculate() {
+  previousValue = Number(previousValue);
+  currentValue = Number(currentValue);
 
-
-function calculate(){
-
-previousValue = Number(previousValue); 
-currentValue = Number(currentValue); 
-
-  if(operator === "+"){
-    previousValue += currentValue; 
-  } else if(operator === "-"){
-    previousValue -= currentValue; 
-  } else if(operator === "x"){
-    previousValue *= currentValue; 
+  if (operator === "+") {
+    previousValue += currentValue;
+  } else if (operator === "-") {
+    previousValue -= currentValue;
+  } else if (operator === "x") {
+    previousValue *= currentValue;
   } else {
-    previousValue /= currentValue; 
+    previousValue /= currentValue;
   }
 
-previousValue = roundNumber(previousValue); 
-previousValue = previousValue.toString(); 
-currentValue = previousValue.toString(); 
-
+  previousValue = roundNumber(previousValue);
+  previousValue = previousValue.toString();
+  currentValue = previousValue.toString();
 }
 
-function roundNumber(num){
-  return Math.round(num * 1000)/1000; 
+function roundNumber(num) {
+  return Math.round(num * 1000) / 1000;
 }
 
-
-
+function addDecimal() {
+  if (!currentValue.includes(".")) {
+    currentValue += ".";
+  }
+}
