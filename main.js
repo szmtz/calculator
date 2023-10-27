4ad0b1a0e9557bf38d0849d0157b69b09888bfd1
saúl z mtz <97975590+szmtz@users.noreply.github.com>
2023-10-27 16:08:54 +0000

    Update handleOperator function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let operator = "";
-let previousValue = "";
-let currentValue = "";
+let operator = '';
+let previousValue = '';
+let currentValue = '';
 
 document.addEventListener("DOMContentLoaded", function () {
   //store all components in HTML in our JS
@@ -14,39 +14,62 @@ document.addEventListener("DOMContentLoaded", function () {
   let previousScreen = document.querySelector(".previous");
   let currentScreen = document.querySelector(".current");
 
-  numbers.forEach((number) =>
+numbers.forEach((number) =>
     number.addEventListener("click", function (e) {
       handleNumber(e.target.textContent);
       currentScreen.textContent = currentValue;
-    })
-  );
-
-  operators.forEach((op) =>
-    op.addEventListener("click", function (e) {
-      handleOperator(e.target.textContent);
-      previousScreen.textContent = previousValue + " " + operator;
-      currentScreen.textContent = currentValue;
     }))
 
+operators.forEach((op) =>
+    op.addEventListener("click", function (e) {
+      handleOperator(e.target.textContent);
+//       previousScreen.textContent = previousValue + " " + operator;
+//       currentScreen.textContent = currentValue;
+     }))
+    })
 
-clear.addEventListener("click", function(){
-  previousValue = '';
-  currentValue = '';
-  operator = '';
-  previousScreen.textContent = currentValue;
-  currentScreen.textContent = currentValue;
-})
-})
+
+// clear.addEventListener("click", function(){
+//   previousValue = '';
+//   currentValue = '';
+//   operator = '';
+//   previousScreen.textContent = currentValue;
+//   currentScreen.textContent = currentValue;
+// })
+// })
 
 
 function handleNumber(num) {
-  if (currentValue.length <= 5) {
-    currentValue += num;
+   if (currentValue.length <= 5) {
+    currentValue += num; 
   }
 }
 
-function handleOperator(op) {
-  operator = op;
-  previousValue = currentValue;
-  currentValue = "";
+function handleOperator(op){
+  operator = op; 
+  previousValue = currentValue; 
+  currentValue = ""; 
 }
+
+
+// function handleOperator(op) {
+//   operator = op;
+//   previousValue = currentValue;
+//   currentValue = ""; 
+// } 
+// function calculate(){
+//   previousValue = Number(previousValue); 
+//   currentValue = Number(currentValue); 
+
+//   if(operator === "+"){
+//     previousValue += currentValue; 
+//   } else if(operator === "-"){
+//     previousValue -= currentValue; 
+//   } else if(operator === "x"){
+//     previousValue *= currentValue; 
+//   } else {
+//     previousValue /= currentValue; 
+//   }
+// console.log(previousValue);
+
+// }
