4fd8b01f184032c319b9bb163f3608becb98157b
saúl z mtz <97975590+szmtz@users.noreply.github.com>
2023-10-27 13:16:43 +0000

    Update CLEAR function

--- main.js
+++ main.js
@@ -18,35 +18,35 @@ document.addEventListener("DOMContentLoaded", function () {
     number.addEventListener("click", function (e) {
       handleNumber(e.target.textContent);
       currentScreen.textContent = currentValue;
+    })
+  );
+
+  operators.forEach((op) =>
+    op.addEventListener("click", function (e) {
+      handleOperator(e.target.textContent);
+      previousScreen.textContent = previousValue + " " + operator;
+      currentScreen.textContent = currentValue;
     }))
-  
-operators.forEach((op) => op.addEventListener("click", function(e){
-  handleOperator(e.target.textContent)
 
-  // previousScreen.textContent = previousValue + " " + operator; 
-  // currentScreen.textContent = currentValue; 
-}))
+
+clear.addEventListener("click", function(){
+  previousValue = '';
+  currentValue = '';
+  operator = '';
+  previousScreen.textContent = currentValue;
+  currentScreen.textContent = currentValue;
 })
-
-// clear.addEventListener("click", function(){
-//   previousValue = ''; 
-//   currentValue = ''; 
-//   operator = ''; 
-//   previousScreen.textContent = currentValue; 
-//   currentScreen.textContent = currentValue; 
-// })
-
- 
+})
 
 
 function handleNumber(num) {
-  if(currentValue.length <= 5){
+  if (currentValue.length <= 5) {
     currentValue += num;
   }
 }
 
-function handleOperator(op){  
- operator = op; 
- previousValue = currentValue; 
- currentValue = ''; 
+function handleOperator(op) {
+  operator = op;
+  previousValue = currentValue;
+  currentValue = "";
 }
